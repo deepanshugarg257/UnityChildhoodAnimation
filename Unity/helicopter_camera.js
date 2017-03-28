0f2f2980525d8be077c2cfb98aeda73844ce78d6
Aashay Singhal <aashay225@gmail.com>
2017-03-28 23:24:43 +0530

    uploaded js files

---
+++ Unity/helicopter_camera.js
@@ -0,0 +1,17 @@
+﻿var R : float = 40;
+var StaticCameraY : float = 60;
+var angle : float=0;
+var target : Vector3;
+
+function Update() {
+	target = new Vector3(162.94,15,126);
+	transform.LookAt(target);
+	transform.position.z = 126 - R*Mathf.Cos(angle*(Mathf.PI/180.0));
+	transform.position.x = 162 - R*Mathf.Sin(angle*(Mathf.PI/180.0));
+	angle= angle + 0.5;
+	angle = angle%360;
+}
+
+function LateUpdate() {
+	GetComponent.<Camera>().main.transform.position.y = StaticCameraY;
+}
---
+++ Unity/sitjs1.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var kamar : GameObject;
+var left_knee : GameObject;
+var right_knee: GameObject;
+var left_hip: GameObject;
+var right_hip: GameObject;
+
+var flag : int = 0;
+var Xpos : float;
+var Ypos : float;
+var Zpos : float;
+
+function Start () {
+	Xpos = transform.position.x;
+	Ypos = transform.position.y;
+	Zpos = transform.position.z;
+}
+
+function Update () {
+	if (Input.GetKeyDown ("g")) {
+			flag = 1;
+		}
+
+		if (Input.GetKeyUp ("g")) {
+			flag = 0;
+		}
+
+		if (flag == 1) {
+			Ypos -= 0.01;
+			Xpos += 0.015;	
+			this.transform.position = new Vector3(Xpos, Ypos, Zpos);
+			left_hip.transform.Rotate (0, 0, 1);
+			right_hip.transform.Rotate (0, 0, 1);
+			left_knee.transform.Rotate (0, 0, -1);
+			right_knee.transform.Rotate (0, 0, -1);
+		}
+}
